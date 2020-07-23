634e90e3fc4f3626b504004836d459b007b16901
Leif Tukums <leiftukums@gmail.com>
2020-07-23 17:14:41 -0400

    more changes

---
+++ diveline.js
@@ -0,0 +1,14 @@
+// document.write('<p>Boogaloo!</p>');
+// let x = document.getElementById('diveline');
+// diveline.animationstart = () => {
+//     diveline.innerHTML = 'dive ith us';
+// };
+
+// function localMap () {
+//     let mapProp = {
+//         center:new google.maps.LatLng(51.508742,-0.120850),
+//         zoom: 5,
+//     };
+//     let map = new google.maps.Map(document.getElementById('map'), mapProp);
+// }
+
